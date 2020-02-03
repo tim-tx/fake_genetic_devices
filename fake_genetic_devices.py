6fc9c7deff449ff42005e0447441aa998c1657ec
Tim Jones <tjones01@gmail.com>
2020-02-03 15:52:27 -0500

    Generate 64 output devices.

--- fake_genetic_devices.py
+++ fake_genetic_devices.py
@@ -1,3 +1,47 @@
+import csv
+import json
 from random import choice
 
-print(''.join(choice('ATGC') for i in range(32)))
+
+names = set()
+output = []
+
+with open('FPvisualization/FPs_processed.csv', newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+    for row in csvreader:
+        names.add(row[0])
+
+it = iter(names)
+for i in range(64):
+    output.append({
+        "collection": "output_reporters",
+        "name": next(it) + "_reporter",
+        
+    })
+
+for i in range(64):
+    output.append({
+        "collection": "output_structure",
+        "gate_name": output[i]['name'],
+        "devices": [
+            { "name": output[i]['name'],
+              "components": [
+                  "#promoter?",
+                  "#promoter?",
+                  output[i]['name'].rstrip("reporter") + "cassette"
+              ]
+            }
+        ] 
+    })
+    
+for i in range(64):
+    output.append({
+        "collection": "parts",
+        "type": "cassette",
+        "name": output[i]['name'].rstrip("reporter") + "cassette",
+        "dnasequence": ''.join(choice('ATGC') for i in range(64))
+    })
+
+with open("Eco.64dummy.output.json", "w") as output_file:
+    output_file.write(json.dumps(output, indent=4))
